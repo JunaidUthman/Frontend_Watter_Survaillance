eaa4f9ff21ac52ddaeb1ca54c9f6da9e74e55c51
Junaid <junaiduthman123@gmail.com>
2026-01-15 05:13:23 +0100

    update server.js file

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
+// Serve static files from dist
 app.use(express.static(path.join(__dirname, "dist")));
 
+// For all routes, serve index.html
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
